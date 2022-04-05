cab5abf144ea7cd45800bc9cb2b2cacefa687cbf
marcelzanatta <marcel_zanatta@hotmail.com>
2022-04-04 22:15:12 -0300

    Finalizando Java e Java.lang - Object e String

---
+++ Etapa 2/Java e Java.lang/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -0,0 +1,29 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.*;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+//		System.out.println("x");
+//		System.out.println('x');
+//		System.out.println(false);
+		
+		
+		Object cliente = new Cliente();
+		ContaCorrente cc = new ContaCorrente(22,23);
+		ContaPoupanca cp = new ContaPoupanca(33,44);
+
+		System.out.println(cc);
+		System.out.println(cp);
+		System.out.println(cliente);
+		//println(cliente);
+	}
+
+	private static void println(Object referencia ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
